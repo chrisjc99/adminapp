cb1f138d209a8457dcd19bb6f6d8d4dbdaed069b
Chris Cafferty <130885768+chrisjc99@users.noreply.github.com>
2023-06-06 13:39:23 -0400

    Sign in without dynamo connection

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Amplify, Auth} from 'aws-amplify';
+import awsconfig from './aws-exports';
+import  {withAuthenticator} from '@aws-amplify/ui-react';
+
+Amplify.configure(awsconfig);
 
 function App() {
+  const signOut = async () => {
+    try {
+      await Auth.signOut();
+    } catch (error) {
+      console.log('Error signing out: ', error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={signOut}>Sign Out</button>
+        <h2>My App Content</h2>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
